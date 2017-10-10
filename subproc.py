af691e0a129f5c040f418e5482061d7f9dc263ac
Neeraj Prem Verma <neeraj@MF-neerajprem-173.local>
2017-10-10 17:51:27 +0530

    v2 - with message variable

--- subproc.py
+++ subproc.py
@@ -1,8 +1,9 @@
 import json
 import requests
 
+SlackMessage = "testing emoji codes :simple_smile: "
 webhook_url = 'https://hooks.slack.com/services/XYXYXYXYXY/ABCDEFGHI/123456789012345678901234'
-slack_data = {'text': "My Pyhton code to send messages to Slack's incoming webhook is working"}
+slack_data = {'text': SlackMessage }
 
 response = requests.post( webhook_url, data=json.dumps(slack_data), headers={'Content-Type': 'application/json'})
 print response
